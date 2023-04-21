55a84a90e834a9cb7762bcc5056fba681b2beda1
Kleyal <90624521+Kleyal@users.noreply.github.com>
2023-04-21 22:57:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,6 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    significance_level = 0.06
+    res = proportions_ztest([x_success, y_success],[x_cnt, y_cnt], alternative = 'larger')
+    return res[1] < significance_level
